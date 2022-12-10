457b142e40767d2b82e9ecbc9c2161635fc0698f
Clement Obumneme <you@example.com>
2022-12-09 18:41:45 -0800

    mand

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - print alphabets in lower case followed by a new line, except q and e
+* Return: Always 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
