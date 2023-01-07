300d293697bcda50c3548fc6f3aeaea050577f40
Clement Obumneme <you@example.com>
2023-01-07 12:46:37 -0800

    09

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - Entry point
+ * _puts - something
+ * @str: input
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
